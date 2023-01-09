f55763ea9eebc3984ff88c3d4986e1ac1a7f2c4b
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2023-01-09 18:57:12 +0100

    add markup gallary

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,9 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
-const a = 10;
+console.log(SimpleLightbox);
 
-console.log(a);
-// console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const markupGallary = createGalleryMarkup(galleryItems);
+
+function createGalleryMarkup(items) {
+  const markup = items
+    .map(({ preview, original, description }) => {
+      return `
+        <a class="gallery__item" 
+        href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    `;
+    })
+    .join('');
+
+  return markup;
+}
+
+gallery.insertAdjacentHTML('beforeend', markupGallary);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
