3e2c5f495155d22d1095fd3e1f25fc36423e1557
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2023-01-09 15:50:49 +0100

    add markup

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,5 +5,25 @@ import { galleryItems } from './gallery-items';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// console.log(galleryItems);
+console.log(galleryItems);
 console.log(SimpleLightbox);
+
+const gallery = document.querySelector('.gallery');
+
+const markupGallary = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      ` 
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}/>
+</a>
+      `
+  )
+  .join('');
+
+gallery.insertAdjacentHTML('beforeend', markupGallary);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
