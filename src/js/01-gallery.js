8b0e6907769a8860d60055013ec8eee8ad966ad1
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2023-01-09 15:01:04 +0100

    fix

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,8 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+console.log(SimpleLightbox);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,6 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
+const a = 10;
+console.log(a);
+console.log(Player);
