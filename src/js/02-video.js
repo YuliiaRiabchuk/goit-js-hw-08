af7ab7f846d3b8f9bc34283e9c042ff7aa4d3028
YuliiaRiabchuk <yuliiariabchuk@gmail.com>
2023-01-09 21:30:21 +0100

    add videoplayer

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,22 @@
 import Player from '@vimeo/player';
-// import throttle from 'lodash.throttle';
+import throttle from 'lodash.throttle';
 
-console.log(Player);
+const iframe = document.querySelector('iframe');
+console.log(iframe);
+
+const STORAGE_KEY = 'videoplayer-current-time';
+
+const player = new Player(iframe);
+
+player.on(
+  'timeupdate',
+  throttle(function (runVideoTime) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(runVideoTime.seconds));
+  }, 1000)
+);
+
+const currentTime = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
